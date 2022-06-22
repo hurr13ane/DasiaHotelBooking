4c4fc417de4e414d29f5b514cd9b66bc17adf219
hurr13ane <iercosandianamaria@gmail.com>
2022-06-22 21:14:11 +0300

    -

--- php/config.php
+++ php/config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'dasiaproiect');
+define('DB_USERNAME', 'u388870369_Dasia');
+define('DB_PASSWORD', 'Hurr13ane');
+define('DB_NAME', 'u388870369_dasiaproiect');
  
 /* Attempt to connect to MySQL database */
 $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
